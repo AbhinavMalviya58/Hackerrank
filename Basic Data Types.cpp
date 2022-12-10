b86089b3d0aba9cf424adefe0a9b361cc9c7c037
Abhinav Malviya <cheatpot1@gmail.com>
2022-12-10 18:53:27 +0530

    exp

---
+++ Basic Data Types.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int w;
+    long x;
+    char y;
+    float z;
+    double v;
+    scanf("%d %ld %c %f %lf",&w,&x,&y,&z,&v);
+    printf("%d\n%ld\n%c\n%f\n%lf",w,x,y,z,v);
+
+    return 0;
+}
---
+++ Conditional Statements 2 .cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n ;
+    cin >> n;
+
+    string arr[9] = {"one","two","three","four","five","six","seveen","eight","nine",};
+    if(n>=1 && n<=9)
+        {
+        cout<< arr[n-1]<<endl;
+        }
+    else
+    {
+        cout<<"Greater than 9" <<endl;
+    }
+    return 0;
+}
