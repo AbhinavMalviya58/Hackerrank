af14121e0bb2b2b336c69b41671e5cf4bb6588e5
Abhinav Malviya <abhinavmalviya4321@gmail.com>
2022-12-11 01:34:12 +0530

    11th DEC

--- Java_Strings_Introduction.java
+++ Java_Strings_Introduction.java
@@ -14,14 +14,22 @@ public class Java_Strings_Introduction {
         int size = c.length();
         System.out.println(size);
         /*  For the second line, write Yes if A is lexicographically greater than B otherwise print No instead.  */ 
-        int x = A.length();
-        int y = B.length();
-        if(x < y)
-            System.out.println("Yes");
+        int i = A.compareTo(B);
+        if(i <= 0)
+            System.out.println("NO");
         else
-            System.out.println("No");
+            System.out.println("Yes");
+        /*   capitalize the first letter in both A and B and print them on a single line, separated by a space. */ 
+        int delta = 'a'-'A' ; // +32
+		char ch = A.charAt(0); // ch = 'a'
+		char ah = B.charAt(0); // ch = 'a'
+		// 'a' -> 'A'
+		char val = (char) (ch-delta); // val = char(a-32) = 'A'
+		char valu = (char) (ah-delta); // val = char(a-32) = 'A'
+		String ans = val + A.substring(1);
+		String answ = valu + B.substring(1);
+		System.out.print(ans);
+		System.out.print(" "+answ);
     }
 }
-
-
 
