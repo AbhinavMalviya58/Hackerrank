28ae6e5dcffc687d0618dc9576f2f238f5df330b
Abhinav Malviya <93449696+Harsh444509A@users.noreply.github.com>
2022-12-24 22:00:43 +0530

    24/DEC/2022

--- Java_Substring.java
+++ Java_Substring.java
@@ -1,3 +1,11 @@
+import java.io.*;
+import java.util.*;
 public class Java_Substring {
-    
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        String S = in.next();
+        int start = in.nextInt();
+        int end = in.nextInt();
+        
+    }
 }
