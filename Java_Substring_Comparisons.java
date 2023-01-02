e9ddfa040a890fd1a5814f8b88fefcb890f59dae
Abhinav Malviya <93449696+Harsh444509A@users.noreply.github.com>
2023-01-03 00:23:24 +0530

    3 JAN 2023

---
+++ Java_Substring_Comparisons.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class Java_Substring_Comparisons {
+    public static String getSmallestAndLargest(String s, int k) {
+        String sub = s.substring(0,k);
+        String smallest = sub;
+        String largest = sub;
+        
+        // Complete the function
+        // 'smallest' must be the lexicographically smallest substring of length 'k'
+        // 'largest' must be the lexicographically largest substring of length 'k'
+        for(int i=1; i<=s.length()-k;i++){
+            sub = s.substring(i, i+k);
+            if(sub.compareTo(largest)>0){
+                largest = sub;
+            }
+            if(sub.compareTo(smallest)<0){
+                smallest = sub;
+            }
+        }
+        return smallest + "\n" + largest;
+    }
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.next();
+        int k = scan.nextInt();
+        scan.close();
+        System.out.println(getSmallestAndLargest(s, k));
+    }
+}
